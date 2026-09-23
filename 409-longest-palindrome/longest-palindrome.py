8127a6a47281aaf833fbd776fda579de14901ecc
Ansuman456 <greatassassin98@gmail.com>
2026-09-24 00:38:20 +0530

    Time: 2 ms (53.93%) | Memory: 19.4 MB (28.33%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,16 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         d={}
-        oddfreq=0
+        oddfreq=0 #no of odd freq characters
         for ele in s:
             d[ele]=0
         for ele in s:
             d[ele]+=1
-            if(d[ele]%2!=0):
+            if(d[ele]%2!=0): #on first occurance cosider that oddfreq
                 oddfreq+=1
-            else:
+            else: # on 2nd occurance remove that from oddfreq
                 oddfreq-=1
-        if(oddfreq==0):
+        if(oddfreq==0): #no oddfreq, all even freq
             return len(s)
-        else:
+        else: # include one odd freq char as middle of palindrome
             return len(s)-oddfreq+1
